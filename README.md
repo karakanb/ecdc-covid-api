492b7e886742aeaefc123c7973d61fae3d99ada0
Burak Karakan <burak.karakan@gmail.com>
2020-08-27 00:49:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# ECDC API - Covid Cases per 100k Population
+
+This is a simple API that returns the latest information on the number of cases per 100k population for all the countries from the [European Centre for Disease Prevention and Control](https://opendata.ecdc.europa.eu/covid19/casedistribution/) website.
+
+```json
+[
+  {
+    "dateRep": "2020-08-26T10:00:00.000Z",
+    "casesPer100k": 896.43495438,
+    "country": "Aruba"
+  },
+  {
+    "dateRep": "2020-08-26T10:00:00.000Z",
+    "casesPer100k": 541.96994292,
+    "country": "Turks_and_Caicos_islands"
+  }
+]
+```
---
+++ api/index.js
@@ -0,0 +1,46 @@
+const parse = require('csv-parse')
+const got = require('got');
+
+const CSV_URL = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
+
+module.exports = async (req, res) => {
+  try {
+    const response = await got(CSV_URL);
+    parse(response.body, { columns: true }, (err, rows) => {
+      const countries = {}
+      for (let index = 0; index < rows.length; index++) {
+        const row = rows[index];
+        const rowCountry = row.countriesAndTerritories;
+        
+        // initialize the country information
+        if (!(rowCountry in countries)) {
+          countries[rowCountry] = {
+            dateRep: null,
+            casesPer100k: null,
+            country: null
+          }
+        }
+
+        // if the latest data found, update the previous one.
+        const rowDate = new Date(parseInt(row.year), parseInt(row.month) - 1, parseInt(row.day), 12, 0, 0, 0);
+        if (!countries[rowCountry].dateRep || rowDate > countries[rowCountry].dateRep) {
+          countries[rowCountry] = {
+            dateRep: rowDate,
+            casesPer100k: parseFloat(row['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000']),
+            country: rowCountry
+          }
+        }
+      }
+
+      const countryItems = Object.values(countries);
+      countryItems.sort((a, b) => {
+        return b.casesPer100k - a.casesPer100k;
+      });
+
+      res.setHeader('Cache-Control', 'max-age=0, s-maxage=3600')
+      res.send(countryItems);
+    });
+  } catch (error) {
+    console.log(error.response.body);
+  }
+}
