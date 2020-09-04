2e345737fa19309344d623e124c9e92a13e21e9a
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-09-04 19:23:34 +0300

    add(client): chart container

---
+++ client/src/components/ChartContainer.vue
@@ -0,0 +1,26 @@
+<template>
+   <cv-tabs aria-label='chart tab label'>
+    <cv-tab id='tab-1' label='Last N Days'>
+      <LastNDays />
+    </cv-tab>
+    <cv-tab id='tab-2' label='Country'>
+      <Country />
+    </cv-tab>
+  </cv-tabs>
+</template>
+
+<script>
+import LastNDays from './charts/LastNDays'
+import Country from './charts/Country'
+export default {
+  name: 'ChartContainer',
+  components: {
+    LastNDays,
+    Country
+  }
+}
+</script>
+
+<style>
+
+</style>
